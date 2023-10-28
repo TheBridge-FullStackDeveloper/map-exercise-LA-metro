ba33f50a4a45731c61a08ceb6cfc956527a42e19
Adrián Mijarra <adrian.mijarra@gmail.com>
2023-10-28 13:16:17 +0200

    Mapeado funcional

---
+++ index.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXZhbmVzdGViYW4iLCJhIjoiY2xvOXFiYXdnMGo0NDJqcXByMWp5eGt2NCJ9.M4IK9Cm2MPbyj26ZXeukug';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-118.243683, 34.052235],
+  zoom: 12
+});
