11ad7d491b37c49f7386b9cf65077bc0d5001eb3
Adrián Mijarra <adrian.mijarra@gmail.com>
2023-11-06 12:51:31 +0100

    trenes se mueven

--- index.js
+++ index.js
@@ -4,5 +4,23 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [-118.243683, 34.052235],
-  zoom: 12
+  zoom: 12,
 });
+function trenes() {
+  fetch("https://api.metro.net/LACMTA/vehicle_positions/all")
+    .then((response) => response.json())
+    .then((trenes) => {
+      trenes.forEach((el) => {
+        var marker = new mapboxgl.Marker()
+          .setLngLat([el.position.longitude, el.position.latitude])
+          .setPopup(new mapboxgl.Popup().setHTML(el.vehicle.vehicle_id))
+          .addTo(map);
+      });
+    });
+}
+
+trenes();
+
+// Bonus: Refresca los datos cada 10 segundos
+setInterval(trenes, 10000);
+
